7393b1d4273f44cebb31c02d8ed611a52dc516ca
Aiden Gandhi <90153180+000600@users.noreply.github.com>
2022-04-15 20:58:10 -0600

    Update Heart Classifier.py
    Added comments and deleted unnecessary code.

--- Heart Failure/Heart Classifier.py
+++ Heart Failure/Heart Classifier.py
@@ -12,15 +12,18 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
+# Get dataset
 data = pd.read_csv('Heart Failure Clinical Records.csv')
 df = pd.DataFrame(data)
 
-Y = []
-
+# Remove the 'DEATH_EVENT' value from the dataset for prediction
 data.drop(['DEATH_EVENT', 'time'], axis = 1, inplace = True)
 df2 = pd.DataFrame(data)
-X = []
 
+X = []
+Y = []
+
+# Add the necessary components to X and Y
 for row in range(df2.shape[0]):
   rows = []
   for point in range(11):
@@ -30,17 +33,16 @@ for row in range(df2.shape[0]):
 for row in range(df.shape[0]):
   Y.append(df.iloc[row][-1])
 
-print(X)
-print(Y)
-
+# Divide the X and Y values
 trainx, testx, trainy, testy = train_test_split(X, Y, random_state = 1)
-x = np.array(trainx)
-y = np.array(trainy)
 
+# Create Adam optimizer
 opt = Adam(learning_rate = 0.001)
 
+# Create model
 model = Sequential()
 
+# Add an initial batch norm layer so that all the values are in a reasonable range for the network to process
 model.add(tf.keras.layers.BatchNormalization())
 model.add(Dense(512, input_shape = [11]))
 
@@ -53,21 +55,26 @@ model.add(tf.keras.layers.BatchNormalization())
 model.add(Dense(256, activation = 'relu'))
 model.add(tf.keras.layers.BatchNormalization())
 
+# Output layer
 model.add(Dense(2))
 model.add(Softmax())
 
+# Compile model
 model.compile(optimizer = opt, loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True), metrics=['accuracy'])
-
 early_stopping = EarlyStopping(min_delta = 0.001, patience = 5, restore_best_weights = True)
 
-history = model.fit(X, Y, epochs = 100) # callbacks = [early_stopping]
+# Fit model and store training history
+history = model.fit(trainx, trainy, epochs = 100) # To add callbacks, add the following as a parameter: callbacks = [early_stopping]
 history_df = pd.DataFrame(history.history)
 
+# View the model's loss
 history_df.head()
 history_df.loc[0:, 'loss'].plot()
 
+# Prediction vs. actual value (change the index to view a different input and output set
 print(np.argmax(model.predict([testx[0]])))
 print(testy[0])
 
+# Evaluate the model
 test_loss, test_acc = model.evaluate(np.array(testx), np.array(testy), verbose=2)
 print('Test accuracy:', test_acc)
